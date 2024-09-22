2908ceaebd1c2c0abf3f9ee2c2e9453a2d1ffc20
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2024-09-22 19:02:36 -0300

    feat: adding todo list and create

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="todo-list">
+    <div class="container">
+      <div class="row bg-white mt-5 p-3 rounded">
+        <h3>TODO List do {{name}}</h3>
+        <hr>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="d-flex justify-content-between gap-1 mb-3">
+          <input
+            v-model="newTask"
+            type="text" 
+            class="form-control" 
+            placeholder="Nova tarefa"
+          >
+
+          <button 
+            class="btn btn-primary" 
+            @click="addTask"
+          >
+            Adicionar
+          </button>
+        </div>
+
+        <div class="d-flex flex-column gap-2">
+          <div 
+            class="card"
+            v-for="(task, index) in tasks"
+            :key="index"
+            @click="task.done = !task.done"
+          >
+            <div class="card-body d-flex justify-content-between">
+              <div class="d-flex gap-2 align-items-center">
+                <input type="checkbox" v-model="task.done">
+                <h5 class="card-title m-0">{{task.title}}</h5>
+              </div>
+
+              <p class="card-text fw-bolder">
+                <span v-if="task.done" class="text-success">Concluído</span>
+                <span v-else class="text-danger">Pendente</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { ref } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const name = ref('Matheus');
+const tasks = ref([
+  { title: 'Estudar VueJS', done: false },
+  { title: 'Praticar Vue!', done: false },
+]);
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+const newTask = ref('');
+const addTask = () => {
+  tasks.value.push({ title: newTask.value, done: false });
+  newTask.value = '';
+};
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#todo-list {
+  background-color: #f8f8f8;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
 }
 </style>
